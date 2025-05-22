929fd5fa77517806fbac8b1cf2d8a189dc1a4ae8
Elena Kolevska <kolevska_elena@yahoo.com>
2025-05-22 14:13:52 +0200

    New exercises added

---
+++ Travel_Application/travel_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from travel_app.models import Travel
+
+
+class TravelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TravelForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+    class Meta:
+        model = Travel
+        exclude = ['guide']
